4da84b038d4a1d738ac0895049e71b8e540c6156
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-12-11 01:41:32 +0800

    10.7  - add navbar horizontal scroll

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import Text from './Text';
 import theme from '../theme';
@@ -20,20 +20,22 @@ const styles = StyleSheet.create({
 const AppBar = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.child}>
-                <Link to="/">
-                    <Text color={'textSecondary'} fontSize={'subheading'} fontWeight={'bold'}>
-                        Repositories
-                    </Text>
-                </Link>
-            </View>
-            <View style={styles.child}>
-                <Link to="/signIn">
-                    <Text color={'textSecondary'} fontSize={'subheading'} fontWeight={'bold'}>
-                        Sign in
-                    </Text>
-                </Link>
-            </View>
+            <ScrollView horizontal>
+                <View style={styles.child}>
+                    <Link to="/">
+                        <Text color={'textSecondary'} fontSize={'subheading'} fontWeight={'bold'}>
+                            Repositories
+                        </Text>
+                    </Link>
+                </View>
+                <View style={styles.child}>
+                    <Link to="/signIn">
+                        <Text color={'textSecondary'} fontSize={'subheading'} fontWeight={'bold'}>
+                            Sign in
+                        </Text>
+                    </Link>
+                </View>
+            </ScrollView>
         </View>
     );
 };
